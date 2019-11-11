c2fc0607b3e92bc5493cc1980a728dbe58e76f2d
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-11 22:38:32 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -24,7 +24,9 @@ class Doctor
   end
   
   def patients
-     appointments.map {|appointment| appointment.patient}
+    appointments.map do |appointment|
+      appointment.patient
+    end
   end
   
   
